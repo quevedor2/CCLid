f0644c5e04da9268a0daee82f52a99e299363883
Rene Quevedo <rquevedo@uhnresearch.ca>
2020-01-09 12:50:58 -0500

    Added ability to combine samples at different proportions for genotype and BAF data.  Also, updated the demo function to run through the analysis

--- R/demo_data.R
+++ R/demo_data.R
@@ -1,11 +1,11 @@
-.demoP <- function(data.type, n.sim=5, n.loci=1000, seed=NULL){
+.demoP <- function(data.type, n.sim=5, n.loci=1000, seed=NULL, sd=0.01){
   if(!is.null(seed)) set.seed(seed)
   
   demo.dat <- switch(data.type,
          "BAF"={
            message("Generating randomized 'BAF' data...")
            mus <- c(0.1, 0.25, 0.33, 0.5, 0.66, 0.75, 0.9)
-           sds <- rep(0.01, length(mus))
+           sds <- rep(sd, length(mus))
            lapply(c(1:n.sim), function(i){
              components <- sample(c(1:length(mus)), 
                                   prob=c(0.25, 0.025, 0.05, 0.35, 0.05, 0.025, 0.25),
@@ -16,10 +16,11 @@
          "geno"=  {
            message("Generating randomized 'Genotype' data...")
            lapply(c(1:n.sim), function(i) {
-             p <- data.frame("0"=round(runif(n = n.loci, min=0, max=0.5),2),
-                             "1"=round(runif(n = n.loci, min=0, max=0.5),2), 
-                             check.names=F)
-             p$`2` <- 1 - rowSums(p)
+             r <- rbinom(n = n.loci, size = 2, prob = 0.5)
+             p <- matrix(rep(0.10, n.loci*3), ncol=3)
+             for(i in seq_along(r)){
+               p[i,r[i]] <- 0.8
+             }
              p
            })
          })
@@ -61,14 +62,46 @@ genDemoData <- function(data.type='BAF', n.pop=10, ...){
   list("matrix"=demo.mat, "prob"=demo.dat)
 }
 
-combineSamples <- function(){
-  
+combineSamples <- function(data.type, sample.mat, prop){
+  switch(data.type, 
+         'BAF'=apply(sample.mat, 1, weighted.mean, w=prop),
+         'geno'=apply(sample.mat, 1, sample, size=1, prob=prop))
 }
 
-demo <- genDemoData(data.type='BAF', n.sim=5, n.pop=10, n.loci=1000, seed=1)
-test.sample <- demoSample('BAF', 1, demo$prob)
-test.sample <- annoDemoMat(c(1), test.sample)
-
-apply(demo$matrix, 2, function(i) cor(test.sample[,1], i))
-
-corrplot::corrplot(cor(demo$matrix))
+.demo <- function(){
+  data.type <- 'geno'
+  data.type <- 'BAF'
+  s.idx <- c(1,5)
+  prop <- c(0.2, 0.8)
+  
+  demo <- genDemoData(data.type=data.type, n.sim=20, 
+                      n.pop=40, n.loci=1000, seed=1234, sd=0.1)
+  
+  test.sample <- demoSample(data.type, s.idx, demo$prob)
+  test.sample <- annoDemoMat(s.idx, test.sample)
+  if(length(s.idx) > 1){
+    sample.x <- as.matrix(combineSamples(data.type, test.sample, prop=prop))
+  } else {
+    sample.x <- test.sample
+  }
+  
+  concordance <- apply(demo$matrix, 2, function(i) {
+    switch(data.type, 
+           "BAF"=cor(sample.x[,1], i),
+           "geno"=1-philentropy::distance(t(data.frame(sample.x[,1], i)), 
+                                          method='jaccard'))
+  })
+  head(as.matrix(sort(concordance, decreasing = T)), n=15)
+  
+  sim <- apply(demo$matrix, 2, function(i){
+    apply(demo$matrix, 2, function(j){
+      switch(data.type,
+             "BAF"=cor(i,j),
+             "geno"=1-philentropy::distance(t(data.frame(i, j)), 
+                                          method='jaccard'))
+    })
+  })
+  sim[sim == 1] <- NA
+  gplots::heatmap.2(sim, trace = 'none',  dendrogram = 'none', 
+                    cexRow = 0.4, cexCol=0.4, key = FALSE)
+}
