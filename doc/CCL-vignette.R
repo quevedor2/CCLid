cb3c9f979047e25e6e7ed8c7aa17342e0782461e
Rene Quevedo <quever@samwise.(none)>
2020-06-06 01:01:53 -0400

    Building the first vignette with introduction

--- doc/CCL-vignette.R
+++ doc/CCL-vignette.R
@@ -4,6 +4,10 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----setup--------------------------------------------------------------------
-library(CCLid)
+## ----install-pkg, eval=FALSE, results='hide'----------------------------------
+#  library(devtools)
+#  devtools::install_github('quevedor2/CCLid')
+
+## ---- eval=FALSE, results='hide'----------------------------------------------
+#  library(PharmacoGx)
 
