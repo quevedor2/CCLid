d1ac6686d5c8d2f9a4e05b86012c89492c6dd79d
Rene Quevedo <rquevedo@uhnresearch.ca>
2020-06-26 22:38:52 -0400

    - modified download_ref/downloadRefCCL function to download all meta file and the .bin and .desc. - redirects the .desc file to the location of the .bin file - Added a function to check if the rad exists and download else

---
+++ R/downloadRefCCL.R
@@ -0,0 +1,70 @@
+#' downloadRefCCL
+#' @description Downloads the precomputed RDS data structures for reference
+#' 
+#' @param name Currently only supports "BAF"
+#' @param saveDir Directory containign bigmemory .desc/.bin folder, or where to save new download
+#' @param verbose Verbose (Default=FALSE)
+#' @param bin.size Genomic size to chunk the SNPs into
+#' @importFrom bigmemory attach.big.matrix
+#' 
+#' @export
+#'
+downloadRefCCL <- function (name, saveDir = file.path(".", "CCLid"), 
+                            verbose = FALSE, bin.size=NULL) {
+  if (!file.exists(saveDir)) {
+    dir.create(saveDir, recursive = TRUE)
+  }
+  
+  if(!any(toupper(name) %in% CCLid::ccl_table$`Data.type`)){
+    stop("'name' must be one of the following data types: ", 
+         paste(CCLid::ccl_table$`Data.type`, collapse=", "), ".\n", 
+         "Use ?ccl_table for more information on the different data types.")
+  }
+  
+  whichx <- match(toupper(name), CCLid::ccl_table$`Data.type`)
+  refFileName <- paste0(toupper(name), ".rds")
+  
+  ## Load in the probeset BAF/Genotype matrix
+  if(name %in% c("BAF", "GENO")){
+    identifier <- paste0("ref_", tolower(name), "-", as.integer(bin.size))
+    row_ids <- paste0("ref_", tolower(name), "-ids.rds")
+    desc_chk <- file.exists(file.path(saveDir, paste0(identifier, ".desc")))
+    bin_chk <- file.exists(file.path(saveDir, paste0(identifier, ".bin")))
+    row_chk <- file.exists(file.path(saveDir, row_ids))
+    
+    ## Check if the files exist, else download them
+    if (!(desc_chk & bin_chk & row_chk)) {
+      message("Downloading .bin, .desc, and row_ids file...")
+      # URL: .bin file
+      downloader::download(url = as.character(CCLid::ccl_table[whichx, "URL"]), 
+                           destfile = file.path(saveDir, paste0(identifier, ".bin")), 
+                           quiet = !verbose)
+      
+      # URL2: .desc file
+      downloader::download(url = as.character(CCLid::ccl_table[whichx, "URL2"]), 
+                           destfile = file.path(saveDir, paste0(identifier, ".desc")), 
+                           quiet = !verbose)
+      
+      # URL3: row IDs file
+      downloader::download(url = as.character(CCLid::ccl_table[whichx, "URL3"]), 
+                           destfile = file.path(saveDir, row_chk), 
+                           quiet = !verbose)
+    }
+    
+    ## Read in bigmemory stuff                      
+    if(verbose) print("Reading in bigmemory sample x probeset object...")
+    shared.desc <- dget(file.path(saveDir, paste0(identifier, ".desc")))
+    shared.desc@description$dirname <- gsub("\\/?$", "/", saveDir)
+    shared.desc@description$filename <- paste0(identifier, ".bin")
+    shared.bigobject <- attach.big.matrix(shared.desc)
+    ref.mat <- shared.bigobject
+    options(bigmemory.allow.dimnames=TRUE)
+    
+    ids <- readRDS(file.path(saveDir, row_ids))
+    rownames(ref.mat) <- ids
+    
+    return(ref.mat)
+  } else {
+    .chkAndDownload(name, whichx, saveDir)
+  }
+}
